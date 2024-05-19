e6a64809abcf84ddb3c55fdda3e069aca21f7c69
jesus.acevedo <antoniozkt@gmail.com>
2024-05-19 11:14:35 -0500

    feat: Refactor sidebar component and update welcome message styling

--- app/components/chat/sidebar.tsx
+++ app/components/chat/sidebar.tsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react";
-import { CircleImage } from "../common/circle-image";
+import { Fragment, useEffect, useState } from "react";
 import { UserName } from "./username";
-import WelcomeUser from "./welcome";
 import { createChat, getChats } from "@/services/chatService";
 import { ChatApiResponse } from "@/types/Chat";
+import WelcomeUser from "./welcome";
 
 export function Sidebar() {
   const [chats, setChats] = useState([]);
@@ -12,7 +11,9 @@ export function Sidebar() {
     try {
       const chats = await getChats("1f14337c-3065-4a20-9e6c-8d844dde6bed");
       setChats(chats);
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
@@ -21,24 +22,25 @@ export function Sidebar() {
 
   return (
     <aside className="flex flex-col justify-center items-center bg-slate-950 py-10 px-10 h-full rounded-sm">
-      <button
-        className="bg-white rounded-sm p-2"
-        onClick={() => createChat("1f14337c-3065-4a20-9e6c-8d844dde6bed")}
-      >
-        New chat
-      </button>
-      <h3 className="text-white">Chats:</h3>
-      {chats.map((chat: ChatApiResponse) => (
-        <div key={chat.chatId} className="flex flex-row gap-x-2 items-center">
-          <h2 className="text-3xl font-bold text-white">{chat.timestamp}</h2>
-        </div>
-      ))}
-      <div className="flex flex-col justify-center items-center gap-y-8 flex-grow">
-        <div className="flex flex-row gap-x-2 items-center">
-          <CircleImage src={"/icons/robot-profile.svg"} alt={"ReflexBot"} />
-          <h1 className="text-3xl font-bold text-white">ReflexBot</h1>
-        </div>
+      <div className="flex flex-col justify-center items-center gap-y-8">
         <WelcomeUser />
+        <button
+          type="button"
+          className="bg-[#1d252d] text-[#00bf6f] hover:bg-[#404041] rounded-sm p-2 font-bold w-full"
+          onClick={() => createChat("1f14337c-3065-4a20-9e6c-8d844dde6bed")}
+        >
+          New chat
+        </button>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-y-2 flex-grow">
+        {chats.map((chat: ChatApiResponse) => (
+          <Fragment key={chat.chatId}>
+            <h3 className="text-white font-semibold text-xl">Chats</h3>
+            <button className="text-lg font-bold bg-slate-500 hover:bg-slate-700 text-white p-2 rounded-sm">
+              {new Date(chat.timestamp).toLocaleString()}
+            </button>
+          </Fragment>
+        ))}
       </div>
       <UserName />
     </aside>
--- app/components/chat/welcome.tsx
+++ app/components/chat/welcome.tsx
@@ -1,8 +1,17 @@
+import { Fragment } from "react";
+import { CircleImage } from "../common/circle-image";
+
 export function WelcomeUser() {
   return (
-    <p className="text-white max-w-64">
-      Make new connections and share experiences. ✨
-    </p>
+    <Fragment>
+      <div className="flex flex-row gap-x-2 items-center">
+        <CircleImage src={"/icons/robot-profile.svg"} alt={"ReflexBot"} />
+        <h1 className="text-3xl font-bold text-white">ReflexBot</h1>
+      </div>
+      <p className="text-white max-w-64">
+        Make new connections and share experiences. ✨
+      </p>
+    </Fragment>
   );
 }
 
