e8b295d7fed1ccd0fa6dc994841721db0ed8b515
jesus.acevedo <antoniozkt@gmail.com>
2024-05-21 01:56:00 -0500

    feat: Refactor chat component

---
+++ app/components/chat/chat-content.tsx
@@ -0,0 +1,38 @@
+import { RefObject } from "react";
+import { Message } from "@/types/Message";
+import { Messages } from "./messages";
+import { EnumAuthor } from "@/db/enum/message";
+import { ChatForm } from "./chat-form";
+
+interface ChatContentProps {
+  chatId: string | null;
+  messages: Message[] | null;
+  messagesEndRef: RefObject<HTMLDivElement>;
+}
+
+export const ChatContent = ({
+  chatId,
+  messages,
+  messagesEndRef,
+}: ChatContentProps) => (
+  <div className="flex flex-col justify-end items-center gap-y-2 h-full pb-5 max-h-[87vh] w-[450px]">
+    <main
+      className="flex flex-col justify-start pt-4 items-center gap-x-5 gap-y-5 overflow-y-auto w-full max-w-md"
+      role="main"
+    >
+      {chatId && messages?.length === 0 && (
+        <p className="text-gray-500">No messages yet</p>
+      )}
+      {messages?.map((message) => (
+        <Messages
+          key={message.id}
+          message={message.content}
+          timestamp={message.timestamp}
+          isBotMessage={message.author === EnumAuthor.BOT}
+        />
+      ))}
+      <div className="none" ref={messagesEndRef} />
+    </main>
+    <ChatForm />
+  </div>
+);
--- app/components/chat/chat.tsx
+++ app/components/chat/chat.tsx
@@ -1,11 +1,9 @@
 "use client";
 import { useContext, useEffect, useRef } from "react";
 import Footer from "../common/footer";
-import { ChatForm } from "./chat-form";
 import { Sidebar } from "./sidebar";
-import { Messages } from "./messages";
-import { EnumAuthor } from "@/db/enum/message";
 import { ChatContext } from "@/app/chat/page";
+import { ChatContent } from "./chat-content";
 
 export function Chat() {
   let { chatId, messages } = useContext(ChatContext);
@@ -23,26 +21,11 @@ export function Chat() {
       <div className="flex justify-center items-center h-full gap-x-10 my-10">
         <div className="flex h-full bg-white gap-x-2 rounded-sm">
           <Sidebar />
-          <div className="flex flex-col justify-end items-center gap-y-2 h-full pb-5 max-h-[87dvh] w-[450px]">
-            <main
-              className="flex flex-col justify-start pt-4 items-center gap-x-5 gap-y-5 overflow-y-auto w-full max-w-md"
-              role="main"
-            >
-              {chatId && messages?.length === 0 && (
-                <p className="text-gray-500">No messages yet</p>
-              )}
-              {messages?.map((message) => (
-                <Messages
-                  key={message.id}
-                  message={message.content}
-                  timestamp={message.timestamp}
-                  isBotMessage={message.author === EnumAuthor.BOT}
-                />
-              ))}
-              <div className="none" ref={messagesEndRef} />
-            </main>
-            <ChatForm />
-          </div>
+          <ChatContent
+            chatId={chatId}
+            messages={messages}
+            messagesEndRef={messagesEndRef}
+          />
         </div>
       </div>
       <Footer />
