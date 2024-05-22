91452cdd0d7de386bb2a51817d5a51133a979d08
jesus.acevedo <antoniozkt@gmail.com>
2024-05-21 22:15:04 -0500

    feat: add accessibility tags

--- app/components/sidebar/username.tsx
+++ app/components/sidebar/username.tsx
@@ -18,20 +18,21 @@ export function UserName({ userName }: UserNameProps) {
   };
 
   return (
-    <div className="flex flex-row gap-x-2 items-center text-white rounded-sm mt-auto capitalize w-full justify-between">
+    <div className="flex flex-row gap-x-2 items-center text-white rounded-sm mt-auto capitalize w-full justify-between" role="contentinfo">
       <div className="flex items-center gap-x-2">
         <CircleImage
           src={"/icons/cat-user-profile-white.svg"}
           alt={"User profile photo - welcome"}
         />
-        <h3 className="font-bold">{userName}</h3>
+        <h3 className="font-bold" id="userName">{userName}</h3>
       </div>
       <button
         className="flex items-center gap-x-2 hover:border-white hover:rounded-sm hover:font-bold"
         onClick={() => deleteUserCookies()}
+        aria-labelledby="logoutButton"
       >
         <Image src="/icons/logout.svg" width={28} height={28} alt="logout" />
-        <h2>Logout</h2>
+        <h2 id="logoutButton">Logout</h2>
       </button>
     </div>
   );
--- app/components/sidebar/welcome-user.tsx
+++ app/components/sidebar/welcome-user.tsx
@@ -4,11 +4,11 @@ import { CircleImage } from "../common/circle-image";
 export function WelcomeUser() {
   return (
     <Fragment>
-      <div className="flex flex-row gap-x-2 items-center">
+      <div className="flex flex-row gap-x-2 items-center" role="banner">
         <CircleImage src={"/icons/robot-profile.svg"} alt={"ReflexBot"} />
-        <h1 className="text-3xl font-bold text-white">ReflexBot</h1>
+        <h1 className="text-3xl font-bold text-white" id="welcomeTitle">ReflexBot</h1>
       </div>
-      <p className="text-white max-w-80 text-balance text-center">
+      <p className="text-white max-w-80 text-balance text-center" id="welcomeMessage" aria-labelledby="welcomeTitle">
         Welcome! Let&apos;s talk about emotional intelligence. Ask me anything 🎊!
       </p>
     </Fragment>
